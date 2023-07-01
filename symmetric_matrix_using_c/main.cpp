cb7e9b58049054a98d859270188300a5202945e1
ubyhzargam <ubyhzargam@gmail.com>
2023-07-01 14:45:10 +0530

    Initial Commit

---
+++ symmetric_matrix_using_c/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  symmetric_matrix_using_c
+//
+//  Created by Uby H on 01/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
